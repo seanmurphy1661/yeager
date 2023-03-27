bc772a521fbdc1bd369656dd62716b649cf30e82
Sean Murphy <seanmurphy1661@gmail.com>
2023-03-26 21:08:07 -0400

    Dictionary Test

--- function_test.py
+++ function_test.py
@@ -5,15 +5,14 @@ n = 15
 m = 20 
 
 
-test_list = [generate_range_check(n,10,20),generate_required_check(n)]
+test_list = {
+    'range': generate_range_check(n,10,20),
+    'required': generate_required_check(n)
+}
 
 z = generate_range_check(n,10,20)
 x = generate_required_check(n)
 
-l = z(m)
-
-print(l)
-
-print(f"Range check {test_list[0](40)}")
+print(f"Range check {test_list['range'](40)}")
 print(f"Data Present: {x('')}")
 print (z)
